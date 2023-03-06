QUARK-framework/QUARK

commit 90bf9edc9e523ac9ce5eb3092c66e216240dff87

improving configuration
* unique default name for AWS bucket
* more options for pennylane QAOA
* updated support for braket devices

src/config.py
@@ -16,6 +16,14 @@ import configparser
 from pathlib import Path
 import os
 import logging
+import datetime
+import os
+
+run_timestamp=datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+username=os.getlogin()
+bucket_name = 'amazon-braket-benchmark-framework-{}-{}'.format(run_timestamp, username)
+#bucket_name = 'quark234234'
+print(bucket_name)
 
 """
 TODO check if this file is needed in this form!
@@ -37,4 +45,4 @@ else:
 if "BRAKET" not in config:
     config["BRAKET"] = {}
     #defaults
-    config["BRAKET"]["BUCKET"] = 'amazon-braket-benchmark-framework'
+    config["BRAKET"]["BUCKET"] = bucket_name

src/solvers/PennylaneQAOA.py
@@ -32,7 +32,10 @@ from devices.braket.SV1 import SV1
 from devices.braket.TN1 import TN1
 from devices.HelperClass import HelperClass
 from solvers.Solver import *
-
+import logging
+logging.basicConfig(level=logging.DEBUG)
+import boto3
+boto3.set_stream_logger('boto3.resources', logging.DEBUG)
 
 class PennylaneQAOA(Solver):
     """
@@ -47,7 +50,8 @@ class PennylaneQAOA(Solver):
         self.device_options = ["arn:aws:braket:::device/quantum-simulator/amazon/sv1",
                                "arn:aws:braket:::device/quantum-simulator/amazon/tn1",
                                "arn:aws:braket:::device/qpu/ionq/ionQdevice",
-                               "arn:aws:braket:::device/qpu/rigetti/Aspen-9",
+                               "arn:aws:braket:us-west-1::device/qpu/rigetti/Aspen-M-2",
+                               "arn:aws:braket:::device/quantum-simulator/amazon/sv1",
                                "braket.local.qubit",
                                "default.qubit",
                                "default.qubit.autograd",
@@ -62,8 +66,8 @@ class PennylaneQAOA(Solver):
             return SV1("SV1", "arn:aws:braket:::device/quantum-simulator/amazon/sv1")
         elif device_option == "arn:aws:braket:::device/quantum-simulator/amazon/tn1":
             return TN1("TN1", "arn:aws:braket:::device/quantum-simulator/amazon/tn1")
-        elif device_option == "arn:aws:braket:::device/qpu/rigetti/Aspen-9":
-            return Rigetti("Rigetti", "arn:aws:braket:::device/qpu/rigetti/Aspen-9")
+        elif device_option == "arn:aws:braket:us-west-1::device/qpu/rigetti/Aspen-M-2":
+            return Rigetti("Rigetti", "arn:aws:braket:us-west-1::device/qpu/rigetti/Aspen-M-2")
         elif device_option == "braket.local.qubit":
             return HelperClass("braket.local.qubit")
         elif device_option == "default.qubit":
@@ -92,7 +96,7 @@ class PennylaneQAOA(Solver):
                                             "description": "How many shots do you need?"
                                         },
                                         "iterations": {  # number measurements to make on circuit
-                                            "values": [10, 20, 50, 75],
+                                            "values": [1, 10, 20, 50, 75],
                                             "description": "How many iterations do you need?"
                                         },
                                         "layers": {
@@ -116,11 +120,11 @@ class PennylaneQAOA(Solver):
                 "description": "How many shots do you need?"
             },
             "iterations": {  # number measurements to make on circuit
-                "values": [10, 20, 50, 75],
+                "values": [1, 10, 20, 50, 75],
                 "description": "How many iterations do you need?"
             },
             "layers": {
-                "values": [2, 3, 4],
+                "values": [1, 2, 3, 4],
                 "description": "How many layers for QAOA do you want?"
             },
             "coeff_scale": {

commit 137fcb0938dad216285156cf1478b0ca1d2fc5c2

use better way to get username
os.getlogin() doesn't work with Windows-Subsystem for Linux (WSL) and/or Docker.
In a Docker container hosted on a Linux host, the file os.getlogin() tries to read the user from (/proc/self/loginuid) contains a value of '-1',
i.e. no user is set. This throws the error:
"OSError: [Errno 6] No such device or address"
In WSL and a Docker container hosted from Windows with WSL, the file simply doesn't exist and os.getlogin() throws:
"FileNotFoundError: [Errno 2] No such file or directory"

Fixes: 90bf9edc9e52 ("improving configuration")

Signed-off-by: Martin Meilinger <martin.meilinger@st.oth-regensburg.de>

src/config.py
@@ -18,15 +18,10 @@ import os
 import logging
 import datetime
 import os
+import getpass
 
 run_timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-
-try:
-    username = os.getlogin()
-except Exception as e:
-    # TODO Handle this in a smarter way in case os.getlogin() fails
-    username = "unknownuser"
-
+username = getpass.getuser()
 bucket_name = 'amazon-braket-benchmark-framework-{}-{}'.format(run_timestamp, username)
 
 """
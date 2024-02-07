154f5ecd88e785f7a1adc9024fbbb723141634b9
Stefan Maschek <stefan-raimund.maschek@atos.net>
2024-02-07 14:52:07 +0100

    autoformat document

--- src/parallel/AsyncCore.py
+++ src/parallel/AsyncCore.py
@@ -13,17 +13,18 @@ from tqpm.devices.qaptiva.myqlm.digital import MyQLMDigitalQPU
 
 class ModuleStage(Enum):
     """enum to classify preprocessing or postprocessing stage of a Core Module"""
+
     PRE = 1
     POST = 2
 
 
 class AsyncCore(Core, ABC):
     """Base class for asynchronous QUARK module.
-    implement submit_preprocess or submit_postprocess analogously to 
+    implement submit_preprocess or submit_postprocess analogously to
     preprocess or postprocess to make use of the asynchronous functionality
-    additionally 
-    
-    
+    additionally
+
+
     implement
     ```
     def submit_preprocess(self, input_data: any, config: dict, **kwargs):
@@ -32,28 +33,35 @@ class AsyncCore(Core, ABC):
         return bar(server_result)
     ```
 
-    instead of 
-    
+    instead of
+
     ```
     def preprocess(self, input_data: any, config: dict, **kwargs):
         server_result = foo()
         return bar(server_result)
     ```
-    
+
     requires the redefinition of MODULE.JobManager, e.g.
     ```
     JobManager = POCAsyncJobManager
     ```
-    
+
     """
-    
+
     JobManager = AsyncJobManager
 
     def get_parameter_options(self) -> dict:
-        return {"async":
-                    {"values": [ModuleStage.PRE.name,ModuleStage.POST.name,"both","all sequencially"],
-                     "description":"Choose which process should run in parallel mode"}
-                }
+        return {
+            "async": {
+                "values": [
+                    ModuleStage.PRE.name,
+                    ModuleStage.POST.name,
+                    "both",
+                    "all sequencially",
+                ],
+                "description": "Choose which process should run in parallel mode",
+            }
+        }
 
     def _prepend_instruction(self, result):
         instruction = Instruction.PROCEED
@@ -61,24 +69,29 @@ class AsyncCore(Core, ABC):
             instruction = Instruction.INTERRUPT
         return instruction, *result
 
-
-    #@ not final, since it is possible to define preprocess async and postprocess sequencially and v.v.           
+    # @ not final, since it is possible to define preprocess async and postprocess sequencially and v.v.
     def preprocess(self, input_data: any, config: dict, **kwargs) -> (any, float):
         """preprocess must not be overwritten in an async module. Use submit_preprocess
         instead"""
-        return self._prepend_instruction(self._process(ModuleStage.PRE, input_data, config, **kwargs))
+        return self._prepend_instruction(
+            self._process(ModuleStage.PRE, input_data, config, **kwargs)
+        )
 
-    #@ not final, since it is possible to define preprocess async and postprocess sequencially and v.v.          
+    # @ not final, since it is possible to define preprocess async and postprocess sequencially and v.v.
     def postprocess(self, input_data: any, config: dict, **kwargs) -> (any, float):
         """postprocess must not be overwritten in an async module. Use submit_postprocess
         instead"""
-        return self._prepend_instruction(self._process(ModuleStage.POST,input_data, config, **kwargs))
-        
-    def _process(self, stage: ModuleStage, input_data: any, config: dict, **kwargs) -> (any, float): 
-        """ Input data is the job
+        return self._prepend_instruction(
+            self._process(ModuleStage.POST, input_data, config, **kwargs)
+        )
+
+    def _process(
+        self, stage: ModuleStage, input_data: any, config: dict, **kwargs
+    ) -> (any, float):
+        """Input data is the job
         returns the AsyncJobManager or the result
         """
-        
+
         # check if *process is configured to run asynchron, else fallback to Core
         async_mode = config.get("async", "none")
         if async_mode != "both" and async_mode != stage.name:
@@ -87,64 +100,70 @@ class AsyncCore(Core, ABC):
             if stage == ModuleStage.POST:
                 return super().postprocess(input_data, config, **kwargs)
 
-        asynchronous_job_info =  kwargs.get("asynchronous_job_info", dict())
-        synchron_mode = config.get("async",None) == "all sequencially"
-        prev_run_job_info = None if not asynchronous_job_info else asynchronous_job_info.get("job_info", False)
+        asynchronous_job_info = kwargs.get("asynchronous_job_info", dict())
+        synchron_mode = config.get("async", None) == "all sequencially"
+        prev_run_job_info = (
+            None
+            if not asynchronous_job_info
+            else asynchronous_job_info.get("job_info", False)
+        )
         is_submit_job = not prev_run_job_info
         is_collect_job = not is_submit_job or synchron_mode
-        
-        job_manager = self.JobManager(self.name, input_data,
-                                      config, **kwargs)
+
+        job_manager = self.JobManager(self.name, input_data, config, **kwargs)
         if is_submit_job:
             return self._submit(stage, job_manager)
 
         if is_collect_job:
             logging.info("Resuming previous run with job_info = %s", prev_run_job_info)
-            job_manager.set_info( **prev_run_job_info)
+            job_manager.set_info(**prev_run_job_info)
             return self._collect(stage, job_manager)
 
     def _submit(self, stage: ModuleStage, job_manager: AsyncJobManager):
         """calls the corresponding submit_pre or postprocess function with arguments
         filled from job_manager"""
-        submit = self.submit_preprocess \
-            if stage == ModuleStage.PRE \
-                else self.submit_postprocess
-        job_manager.job_info = submit(job_manager.input, 
-                                      job_manager.config,
-                                      **job_manager.kwargs)
-        
-        self.metrics.add_metric("job_info", 
-                                job_manager.get_json_serializable_info())
-        
+        submit = (
+            self.submit_preprocess
+            if stage == ModuleStage.PRE
+            else self.submit_postprocess
+        )
+        job_manager.job_info = submit(
+            job_manager.input, job_manager.config, **job_manager.kwargs
+        )
+
+        self.metrics.add_metric("job_info", job_manager.get_json_serializable_info())
+
         return job_manager, 0.0
 
     def _collect(self, stage: ModuleStage, job_manager: AsyncJobManager):
         """calls the corresponding collect_pre or postprocess function with arguments
         filled from job_manager"""
-        collect = self.collect_preprocess \
-            if stage == ModuleStage.PRE \
+        collect = (
+            self.collect_preprocess
+            if stage == ModuleStage.PRE
             else self.collect_postprocess
+        )
 
+        #        try:
+        #            while job_manager.status == AsyncStatus.SUBMITTED:
+        #                time.sleep(1)
+        #            if job_manager.status == AsyncStatus.DONE:
+        #                logging.info(f"job {job_manager} done")
+        #        except KeyboardInterrupt: #TODO: this does not work in my (debugging) setup
+        #            pass
+        #
 
-#        try:
-#            while job_manager.status == AsyncStatus.SUBMITTED:
-#                time.sleep(1)
-#            if job_manager.status == AsyncStatus.DONE:
-#                logging.info(f"job {job_manager} done")
-#        except KeyboardInterrupt: #TODO: this does not work in my (debugging) setup
-#            pass
-#
-
-        result = collect(job_manager.result)  # TODO ugly side effect: modifies job_manager.job_info
-        self.metrics.add_metric("job_info",
-                                job_manager.get_json_serializable_info())
+        result = collect(
+            job_manager.result
+        )  # TODO ugly side effect: modifies job_manager.job_info
+        self.metrics.add_metric("job_info", job_manager.get_json_serializable_info())
 
         return result, job_manager.runtime
-    
+
     def submit_preprocess(self, job, config, **kwargs):
         """interface: overwrite this method to a module specific submission.
-        return value is supposed to be the answer of the server call when submitting 
-        
+        return value is supposed to be the answer of the server call when submitting
+
         e.g.
         ```
         qpu = self._get_qpu_plugin()
@@ -152,23 +171,26 @@ class AsyncCore(Core, ABC):
         return server_response
         ```
         """
-        raise NotImplementedError("If you want to run preprocess asynchron, "
-        "you need to implement a submit_preprocess method in {self.__class__.__name__}")
-    
+        raise NotImplementedError(
+            "If you want to run preprocess asynchron, "
+            "you need to implement a submit_preprocess method in {self.__class__.__name__}"
+        )
+
     def collect_preprocess(self, server_result):
         """interface: overwrite this method to a module specific collect-call"""
         return server_result
-    
+
     def submit_postprocess(self, job, config, **kwargs):
-        raise NotImplementedError("If you want to run postprocess asynchron, "
-        "you need to implement a submit_postprocess method in {self.__class__.__name__}")
-    
+        raise NotImplementedError(
+            "If you want to run postprocess asynchron, "
+            "you need to implement a submit_postprocess method in {self.__class__.__name__}"
+        )
+
     def collect_postprocess(self, server_result):
         return server_result
 
 
 class AsyncPOCDevice(AsyncCore):
-
     JobManager = POCJobManager
 
     def __init__(self, device_name):
--- src/parallel/AsyncJob.py
+++ src/parallel/AsyncJob.py
@@ -11,6 +11,7 @@ import time
 class AsyncModeException(Exception):
     pass
 
+
 class AsyncStatus(Enum):
     UNKNOWN = 0
     SUBMITTED = 1
@@ -18,15 +19,19 @@ class AsyncStatus(Enum):
     FAILED = 3
     SYNCHRON = 4
 
+
 class AsyncJobManager(ABC):
     """Base class for modules capable of performing asynchronous jobs.
-    to use it for parallel computing of preprocess(postprocess) the 
+    to use it for parallel computing of preprocess(postprocess) the
     method submit_preprocess(submit_postprocess) has to be overwritten
-    optially, the collect_preprocess(collect_postprocess) can be overwritten 
-    to change 
-    
+    optially, the collect_preprocess(collect_postprocess) can be overwritten
+    to change
+
     """
-    def __init__(self, module_name, input_data: any = None, config=None, **kwargs) -> None:
+
+    def __init__(
+        self, module_name, input_data: any = None, config=None, **kwargs
+    ) -> None:
         self._status = AsyncStatus.UNKNOWN
         self._job_input_data = input_data
         self._job_return_data = None
@@ -35,70 +40,71 @@ class AsyncJobManager(ABC):
         self.metrics = {}
         self.config = config
         self.kwargs = kwargs
-        
+
     def __repr__(self) -> str:
         return f"parallel job with ID={self.job_info.get('id') or 'UNKNOWN'}"
-        
-        
+
     @property
     def module_name(self):
         return self._module_name
-
 
     @property
     def input(self):
         return self._job_input_data
 
     @input.setter
-    def input(self,value):
+    def input(self, value):
         self._job_input_data = value
-        
+
     @property
     def status(self):
-        """The status of the job on the server. In this POC case, the status turns to DONE after  0.001s. 
-        If the status comes from the QLM, then the Connection().get_info(id=job_info["id"]) would be my 
-        idea to resolve the status. this property might be overwritten in a QLM specific child class 
+        """The status of the job on the server. In this POC case, the status turns to DONE after  0.001s.
+        If the status comes from the QLM, then the Connection().get_info(id=job_info["id"]) would be my
+        idea to resolve the status. this property might be overwritten in a QLM specific child class
         """
         return self._get_status()
-    
+
     def _get_status(self):
         """memorizes if status was already FAILED or DONE before"""
         if self._status not in [AsyncStatus.DONE, AsyncStatus.FAILED]:
             self._status = self.get_status()
         return self._status
-        
-    
+
     @abstractmethod
     def get_status(self) -> AsyncStatus:
         """Server specific implementation of how to determine the job status"""
 
-    #@abstractmethod
+    # @abstractmethod
     @property
     def result(self):
         """returns the job result or self, if job is not finished"""
         if self.status == AsyncStatus.DONE:
-            self.set_info(reception_time=datetime.datetime.today().strftime('%Y-%m-%d-%H-%M-%S'))
+            self.set_info(
+                reception_time=datetime.datetime.today().strftime("%Y-%m-%d-%H-%M-%S")
+            )
             return self.get_result()
         if self.status == AsyncStatus.FAILED:
-            self.set_info(reception_time=datetime.datetime.today().strftime('%Y-%m-%d-%H-%M-%S'))
+            self.set_info(
+                reception_time=datetime.datetime.today().strftime("%Y-%m-%d-%H-%M-%S")
+            )
             try:
                 self.get_result()
             except Exception as e:
                 raise AsyncModeException from e
         return self
-    
+
     @abstractmethod
     def get_result(self) -> dict:
-        """ returns the result of the raw input job as dict object"""
-        
+        """returns the result of the raw input job as dict object"""
+
     @property
     def job_info(self) -> dict:
-        """ stores the direct return value of the submission to the server, i.e.
+        """stores the direct return value of the submission to the server, i.e.
         job_info = qpu.submit(...)
         e.g. the info such as as job_id, owner etc as assigned from QLM,
         see also felix' code. This ought to be json serializable"""
         return self._job_info
-   
+
     @job_info.setter
     def job_info(self, value):
         if self._job_info:
@@ -106,94 +112,111 @@ class AsyncJobManager(ABC):
             return
         self._job_info_type = value.__class__.__name__
         self._job_info = {
-            "submission_time": datetime.datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
-            }
+            "submission_time": datetime.datetime.today().strftime("%Y-%m-%d-%H-%M-%S")
+        }
         self._status = AsyncStatus.SUBMITTED
         self._handle_submit_info(value)
         if "id" not in self.job_info:
             self.job_info["id"] = "unknown"
-        #assert "id" in self.job_info, f"The job info does not contain 'id', please modify the"\
+        # assert "id" in self.job_info, f"The job info does not contain 'id', please modify the"\
         #    f"{self.__class__.__name__}._handle_submit_info method and define a unique id"
 
     def _handle_submit_info(self, value):
-        '''
+        """
         simplest case is just to copy the value inside, or modify, if needed
         NOTE: self._job_info['id']  must be set now
         ```
         self._job_info['server_response'] = value
         assert 'id' in self._job_info, "'id' is needed"
         ```
-        '''
+        """
 
-        self._job_info['server_response'] = value
-        
+        self._job_info["server_response"] = value
+
     @property
     def runtime(self):
         """server site runtime in ms"""
         return self.job_info.get("runtime", 0.0)
-        
-    
+
     def _primitivize(self, value):
         # print(f"primitivize {value} of type {type(value)}")
-        if isinstance(value,(bool, str, int, float, type(None))):
+        if isinstance(value, (bool, str, int, float, type(None))):
             return value
-        if isinstance(value,list):
-            return list([self._primitivize(element) for element in value if self._primitivize(element) is not None])
-        if isinstance(value,tuple):
-            return tuple([self._primitivize(element) for element in value if self._primitivize(element) is not None])
+        if isinstance(value, list):
+            return list(
+                [
+                    self._primitivize(element)
+                    for element in value
+                    if self._primitivize(element) is not None
+                ]
+            )
+        if isinstance(value, tuple):
+            return tuple(
+                [
+                    self._primitivize(element)
+                    for element in value
+                    if self._primitivize(element) is not None
+                ]
+            )
         if isinstance(value, dict):
-            return dict({key:self._primitivize(element) for key,element in value.items() if self._primitivize(element) is not None})
+            return dict(
+                {
+                    key: self._primitivize(element)
+                    for key, element in value.items()
+                    if self._primitivize(element) is not None
+                }
+            )
         return f"{type(value)}-object"
-        
-    def set_info(self,**kwargs):
+
+    def set_info(self, **kwargs):
         """method to manually set job_info."""
         self._job_info.update(kwargs)
-    
-        
-    
+
     def get_json_serializable_info(self):
         """Should return a primitive object, that can be filled into the results.json
         and contain all necessary information to retrieve the job from server again
         and some bookkeeping"""
         return self._primitivize(self.job_info)
 
+
 class POCJobManager(AsyncJobManager):
-    
     def get_status(self):
-        """The status of the job on the server. In this POC case, the status turns to DONE after  0.001s. 
-        If the status comes from the QLM, then the Connection().get_info(id=job_info["id"]) would be my 
-        idea to resolve the status. this property might be overwritten in a QLM specific child class 
+        """The status of the job on the server. In this POC case, the status turns to DONE after  0.001s.
+        If the status comes from the QLM, then the Connection().get_info(id=job_info["id"]) would be my
+        idea to resolve the status. this property might be overwritten in a QLM specific child class
         """
-        #runtime = time.time()-self.job_info.get("submission_time_s", 3000000000)
-        #if runtime< 0.01:
+        # runtime = time.time()-self.job_info.get("submission_time_s", 3000000000)
+        # if runtime< 0.01:
         #    return AsyncStatus.SUBMITTED
         # if random.random() < 0.3:
         #     return AsyncStatus.FAILED
         return AsyncStatus.DONE
-    
+
     def _handle_submit_info(self, value):
-        """ stack is a device like object that has the method submit(job: JOB), where 
+        """stack is a device like object that has the method submit(job: JOB), where
         JOB is the raw input to the async job object"""
-        
-        self._job_info['server_response'] = value
-        
-        self._job_info.update({ 
-            'server_response': value,
-            "id": int(random.random()*1000),
-            "owner": "Smasch",
-            "submission_time_s": time.time(),
-            })
-        #dummy submission:
-        dummy_server_mock= f"dummyServer/job{self._job_info['id']}.pkl"
+
+        self._job_info["server_response"] = value
+
+        self._job_info.update(
+            {
+                "server_response": value,
+                "id": int(random.random() * 1000),
+                "owner": "Smasch",
+                "submission_time_s": time.time(),
+            }
+        )
+        # dummy submission:
+        dummy_server_mock = f"dummyServer/job{self._job_info['id']}.pkl"
         if not os.path.exists("dummyServer"):
             os.mkdir("dummyServer")
-        with open(dummy_server_mock, 'wb') as mock_file:
+        with open(dummy_server_mock, "wb") as mock_file:
             pickle.dump(self.job_info, mock_file)
-            logging.info("file %s written in %s", dummy_server_mock, os.getcwd() )
-        
+            logging.info("file %s written in %s", dummy_server_mock, os.getcwd())
+
     def get_result(self) -> dict:
-        dummy_server_mock= f"dummyServer/job{self._job_info['id']}.pkl"
-        with open(dummy_server_mock, 'rb') as mock_file:
+        dummy_server_mock = f"dummyServer/job{self._job_info['id']}.pkl"
+        with open(dummy_server_mock, "rb") as mock_file:
             self._job_info = pickle.load(mock_file)
-            logging.info("file %s loaded", dummy_server_mock )
+            logging.info("file %s loaded", dummy_server_mock)
         return self.job_info.get("server_response", None)

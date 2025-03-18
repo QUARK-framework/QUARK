6c2321386e1363ee76c1f5af8696484197808ec6
Eric Stopfer <eric.stopfer@iis.fraunhofer.de>
2025-03-18 12:43:49 +0100

    Update author of bp in __init__.py

--- src/modules/applications/optimization/bp/__init__.py
+++ src/modules/applications/optimization/bp/__init__.py
@@ -12,4 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-"""Module for bp"""
+"""
+Module for bp
+Contributed by Eric Stopfer (Fraunhofer IIS)
+"""
